ec7a2d9c970f3f54ce63b3c486c4cb2ab242135a
Bruivert <elbrus4@hotmail.com>
2025-06-12 13:11:21 +0200

    Add files via upload

--- adapters/aedas.py
+++ adapters/aedas.py
@@ -21,13 +21,11 @@ def raspar(url):
             precio_texto = atributos.get('price', '0')
             precio_final = 0
             if precio_texto:
-                numeros_del_precio = ''.join(filter(str.isdigit, precio_texto))
-                if numeros_del_precio:
-                    if len(numeros_del_precio) > 2 and numeros_del_precio.endswith("00"):
-                        precio_final = int(numeros_del_precio[:-2])
-                    else:
-                        precio_final = int(numeros_del_precio)
-
+                numeros = ''.join(filter(str.isdigit, precio_texto))
+                if numeros.endswith("00"):
+                    precio_final = int(numeros[:-2])
+                else:
+                    precio_final = int(numeros)
             promociones_encontradas.append({
                 'promocion': nombre_promo,
                 'zona': zona_promo,
@@ -35,10 +33,6 @@ def raspar(url):
                 'dormitorios': atributos.get('bedrooms_from', 0),
                 'url': f"https://www.aedashomes.com/promociones/{atributos.get('slug')}"
             })
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error al contactar la API de AEDAS: {e}")
-    except (json.JSONDecodeError, KeyError) as e:
-        print(f"Error al procesar los datos JSON de AEDAS: {e}")
-
+    except Exception as e:
+        print(f"Error: {e}")
     return promociones_encontradas
